029e62cad66f8cc8c378c2e41d12aeacdd8928af
Mercury0372 <muslim.kgg@gmail.com>
2024-12-10 01:00:53 +0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,13 @@
+function checkForm(el) {
+
+    const name = el.name.value;
+    const pass = el.pass.value;
+    console.log(name + "-" + pass);
+    return false;
+}
+const myCity = {
+    city:'NewYork',
+    popular:'true',
+    country:'USA',
+}
+console.log(myCity);
